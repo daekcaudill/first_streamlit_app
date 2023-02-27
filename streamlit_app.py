78c6275f97e83ec281ab43e1c28bfb655eb1b81b
daekcaudill <126204096+daekcaudill@users.noreply.github.com>
2023-02-27 14:19:47 -0500

    Update streamlit_app.py
    creating fruit choice variable and editing response from api

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,13 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 
-#using normalize to make restponse tabular
+#using normalize to remove json format
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# using normalize to make restponse tabula
 streamlit.dataframe(fruityvice_normalized)
